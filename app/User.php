0ffc1dccdf4a7c73baba7c62d7c83b998caf6730
shriya01 <jain.shriya@fxbytes.com>
2018-09-13 10:33:35 +0530

    Package Installation and integration with database

--- app/User.php
+++ app/User.php
@@ -2,12 +2,12 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Hootlex\Friendships\Traits\Friendable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
+    use Friendable;
 
     /**
      * The attributes that are mass assignable.
